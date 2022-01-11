7fcbc61a1fc4feef0d03354c19b70bcacbe904a3
JaydenPears <vladimir120905@gmail.com>
2022-01-11 21:32:41 +0300

    first commit

---
+++ artificial_intelligence.py
@@ -0,0 +1,3 @@
+class EnemyBot:
+    def __init__(self):
+        pass
---
+++ main_window.py
@@ -0,0 +1,61 @@
+import pygame
+from artificial_intelligence import EnemyBot
+from backend import (load_image, Colony)
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Button:
+    def __init__(self, x, y, width, height):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+
+    def draw_button(self, color_for_button):
+        pass
+
+
+class ColonyAction:
+    def __init__(self):
+        pass
+
+
+class MainWindow:
+    def __init__(self, size, screen):
+        self.fps = 60
+        self.screen = screen
+        self.size = size
+        self.InitUI()
+
+    def InitUI(self):
+        running = True
+
+        clock = pygame.time.Clock()
+        check_draw_buttons = True
+        array_buttons = []
+
+        while running:
+            self.screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            if check_draw_buttons:
+                pass
+            clock.tick(self.fps)
+            pygame.display.flip()
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Колонизация')
+    size = width, height = 1920, 1080
+    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+
+    mw = MainWindow(size, screen)
